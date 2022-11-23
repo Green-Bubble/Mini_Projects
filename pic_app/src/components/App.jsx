2a98f489c8c4cf10de5b4f3dd52256da436abfde
FoReVeR <forever@github.com>
2022-11-23 23:59:18 +0700

    Update App.jsx

--- pic_app/src/components/App.jsx
+++ pic_app/src/components/App.jsx
@@ -3,20 +3,26 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = {
+    images: [],
+  };
+
+  onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID fXqoP6RrRwOJZOC84XDvh4V0aJWvD7sU5WN41odUHKA",
       },
     });
-    console.log(response.data.results);
-  }
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "20px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images} images
       </div>
     );
   }
