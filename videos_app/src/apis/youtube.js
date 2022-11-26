3cbd8521dcfb95535e1e1ed7b3f0ef0c039300cb
FoReVeR <forever@github.com>
2022-11-26 23:26:56 +0700

    adding axios + setting key

---
+++ videos_app/src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyCPq1BjMGkxs0E-4GvBsfK-GSwvAh6k-uw";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResult: 5,
+    key: KEY,
+  },
+});
