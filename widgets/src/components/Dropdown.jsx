4ad6d5535ca7c58a42f6421bb3e61475a5d2c80f
FoReVeR <forever@github.com>
2022-12-02 22:06:01 +0700

    adding useRef and useEffect clean function

--- widgets/src/components/Dropdown.jsx
+++ widgets/src/components/Dropdown.jsx
@@ -1,18 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
 
   useEffect(() => {
-    document.body.addEventListener(
-      "click",
-      () => {
-        setOpen(false);
-      },
-      { capture: true }
-    );
+    const onBodyClick = (e) => {
+      if (ref.current.contains(e.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, { capture: true });
+    };
   }, []);
-  
+
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -25,7 +30,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div className="ui form" ref={ref}>
       <div className="field">
         <label className="label">Select A Color</label>
         <div className={`ui selection dropdown ${open ? "visible active" : ""}`} onClick={() => setOpen(!open)}>
