5a5aec85c7cbc869ba4b59ca86376c20f90627e8
FoReVeR <forever@github.com>
2022-12-07 18:13:50 +0700

    adding Reducer & Action Creator

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from './components/App'
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./components/App";
+import reducers from "./reducers";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+);
