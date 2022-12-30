36304c7b25d850458029ddb130cf579314d16944
FoReVeR <forever@github.com>
2022-12-30 23:17:18 +0700

    practicing reducer action and store

--- redux 0.1/src/index.js
+++ redux 0.1/src/index.js
@@ -1,32 +1,96 @@
-const createStore = () => {
-  let state;
-  let listeners = [];
+import { createStore } from "./redux";
 
-  const getState = () => state;
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-
-    return () => {
-      listeners = listeners.filter((listenerItem) => listenerItem !== listener);
-    };
-  };
-
+// action creator
+const addTodoActionCreator = ({ id, text }) => {
   return {
-    getState,
-    subscribe,
+    type: "ADD_TODO",
+    payload: {
+      id,
+      text,
+      complete: false,
+    },
   };
 };
 
+const deleteTodoActionCreator = (id) => {
+  return {
+    type: "DELETE_TODO",
+    payload: {
+      id,
+    },
+  };
+};
+
+const toggleActionCreator = (id) => {
+  return {
+    type: "TOGGLE_TODO",
+    payload: {
+      id,
+    },
+  };
+};
+
+// reducer
+const todoReducer = (todos = [], action = {}) => {
+  if (action.type === "ADD_TODO") {
+    return [...todos, action.payload];
+  }
+
+  if (action.type === "DELETE_TODO") {
+    return todos.filter((todo) => todo.id !== action.payload.id);
+  }
+
+  if (action.type === "TOGGLE_TODO") {
+    return todos.map((todo) => {
+      if (todo.id === action.payload.id) {
+        return { ...todo, complete: !todo.complete };
+      }
+      return todo;
+    });
+  }
+
+  return todos;
+};
 
 // consume
-const store = createStore;
+const store = createStore(todoReducer);
 
-store.getState();
+// subscribe state changed
+store.subscribe(() => {
+  console.log("State Changed!", store.getState());
+});
 
-const unsubscribe = store.subscribe(() => {
-  console.log('State Changed');
-  console.log('UI sould be re-render');
-})
+store.dispatch(
+  addTodoActionCreator({
+    id: 1,
+    text: "Learn React",
+  })
+);
 
-unsubscribe();
+store.dispatch(
+  addTodoActionCreator({
+    id: 2,
+    text: "dua",
+  })
+);
+
+store.dispatch(
+  addTodoActionCreator({
+    id: 3,
+    text: "tiga",
+  })
+);
+
+store.dispatch(deleteTodoActionCreator(2));
+store.dispatch(deleteTodoActionCreator(3));
+
+store.dispatch(toggleActionCreator(1));
+
+store.dispatch(
+  addTodoActionCreator({
+    id: 4,
+    text: "empat",
+  })
+);
+
+store.dispatch(toggleActionCreator(4));
