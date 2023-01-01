527bcee114a733811914adb5c9531d337314df7f
FoReVeR <forever@github.com>
2023-01-01 21:38:31 +0700

    adding middleware

--- redux 0.1/src/states/index.js
+++ redux 0.1/src/states/index.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { todoDeletetionCheck } from "./middlewares";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(todoDeletetionCheck));
 
 export { store };
