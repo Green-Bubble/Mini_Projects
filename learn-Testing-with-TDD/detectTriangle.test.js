a960793da134192f30f88f92bcef70b77a875e14
FoReVeR <forever@github.com>
2023-02-06 13:01:54 +0700

    setup jest

---
+++ learn-Testing-with-TDD/detectTriangle.test.js
@@ -0,0 +1,15 @@
+// test scenario untuk detectTriagle
+
+// - detect functiions
+//   - should throw error when input is integer
+//   - should throw error when input is not a positive Number
+//   - should throw error when input is tiangle inequality violation
+//   - should return "segitiga sama sisi" when all input are equal
+//   - should return "segitiga sama kaki" when 2 input are equal
+//   - should return "segitiga sembarang" when all input are different
+
+describe ('detect triangle function', () => {
+  it('should throw error when input is integer', () => {
+    
+  });
+});
