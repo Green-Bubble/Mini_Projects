9604722f1c76925779e6c7b91c617bcd7f4d0b85
FoReVeR <forever@github.com>
2023-02-17 14:23:51 +0700

    adding model->fetch from db

--- php_mvc/app/views/about/index.php
+++ php_mvc/app/views/about/index.php
@@ -1,7 +1,10 @@
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/555.jpg" alt="babon" class="rounded-circle shadow">
 
-  <h1>About Me</h1>
   <p>Halo nama saya
     <?php echo $data['nama'] ?>, umur saya
     <?php echo $data['umur'] ?>, saya adalah
     <?php echo $data['pekerjaan'] ?>
   </p>
+</div>
